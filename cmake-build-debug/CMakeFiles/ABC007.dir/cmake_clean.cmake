938726c699b0d366c3c4c6c84ef3ea11ac192518
umino tomoki <t.umino.nit@gmail.com>
2017-09-21 12:09:16 +0900

    init

---
+++ cmake-build-debug/CMakeFiles/ABC007.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ABC007.dir/main.cpp.o"
+  "ABC007.pdb"
+  "ABC007.exe"
+  "libABC007.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ABC007.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ABC007.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ABC007.
+# This may be replaced when dependencies are built.
